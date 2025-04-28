564be58d2fe43b2aae15dbf6342c3a3467b58a7f
martingarrote <martindambrowskigarrote@gmail.com>
2025-04-27 21:20:01 -0300

    chamar função de inserir vagas ao ouvir evento de reserva criada

--- js/controller/parking-spot-controller.js
+++ js/controller/parking-spot-controller.js
@@ -16,6 +16,7 @@ export default class ParkingSpotController {
 
         document.addEventListener("reservation-created", (event) => {
             this.model.setStatus(event.detail.id, 'occupied')
+            this.insertParkingSpots()
         })
     }
 
